0ef6563d2a877c6216b2a2a9c7adcfc1cf7c0b60
mjenkins <mjenkins@msnpath.com>
2018-01-26 07:08:50 -0500

    added extra information to the client

--- hook.sh
+++ hook.sh
@@ -6,32 +6,82 @@ set -e
 set -u
 set -o pipefail
 
+HOOK="${1}"
+
 case "$1" in
+	"startup_hook")
+		echo ""
+		echo "Startup Hook Executed (${HOOK})"
+	;;
 	"deploy_challenge")
+		DOMAIN="${2}" TOKEN_FILENAME="${3}" TOKEN_VALUE="${4}"
+
 		echo ""
-		echo "Add the following to the zone definition of ${2}:"
-		echo "_acme-challenge.${2}. IN TXT \"${4}\""
+		echo "Deploy Challenge Token"
+		echo "  Add the following TXT record to the zone definition of ${DOMAIN}:"
 		echo ""
-		echo -n "Press enter to continue..."
+		echo "    Name : _acme-challenge.${DOMAIN}"
+		echo "    Value: ${TOKEN_VALUE}"
+		echo ""
+		echo -n "  Press enter to continue..."
 		read tmp
 		echo ""
 	;;
 	"clean_challenge")
+		DOMAIN="${2}" TOKEN_FILENAME="${3}" TOKEN_VALUE="${4}"
+
 		echo ""
-		echo "Now you can remove the following from the zone definition of ${2}:"
-		echo "_acme-challenge.${2}. IN TXT \"${4}\""
+		echo "Clean Challenge Token"
+		echo "  You can remove the following TXT record from the zone definition of ${DOMAIN}:"
 		echo ""
-		echo -n "Press enter to continue..."
+		echo "    Name : _acme-challenge.${DOMAIN}"
+		echo "    Value: ${TOKEN_VALUE}"
+		echo ""
+		echo -n "  Press enter to continue..."
 		read tmp
 		echo ""
 	;;
+	"invalid_challenge")
+		DOMAIN="${2}" RESPONSE="${3}"
+
+		echo ""
+		echo "Invalid Challenge Token"
+	;;
 	"deploy_cert")
-		# do nothing for now
+		DOMAIN="${2}" KEYFILE="${3}" CERTFILE="${4}" FULLCHAINFILE="${5}" CHAINFILE="${6}" TIMESTAMP="${7}"
+		
+		echo ""
+		echo "Deploy Certificate"
+		echo "  Timestamp  : ${TIMESTAMP}"
+		echo "  Certificate: ${CERTFILE}"
+		echo "  Key        : ${KEYFILE}"
+		echo "  Full Chain : ${FULLCHAINFILE}"
+		echo "  Chain      : ${CHAINFILE}"
+		echo ""
 	;;
 	"unchanged_cert")
+		set DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+		echo ""
+		echo "Unchanged Certificate"
+		echo "  The certificate does not need to be updated at this time"
+		echo ""
+		echo "  Existing Certificate Details"
+		echo "    Timestamp  : ${TIMESTAMP}"
+		echo "    Certificate: ${CERTFILE}"
+		echo "    Key        : ${KEYFILE}"
+		echo "    Full Chain : ${FULLCHAINFILE}"
+		echo "    Chain      : ${CHAINFILE}"
+		echo ""
+	;;
+	"request_failure")
 		# do nothing for now
 	;;
+	"exit_hook")
+		echo ""
+		echo "Exit hook executed"
+	;;
 	*)
+		echo ""
 		echo "Unknown hook \"${1}\""
 		exit 1
 	;;
